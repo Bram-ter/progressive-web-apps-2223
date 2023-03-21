0b3e2e3e5f06a2bd59bd40ec176d1080737ab4c9
Bram-ter <bram.tersteeg2@hva.nl>
2023-03-21 16:44:12 +0100

    add foreach in views

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,20 @@
+const fetch = require('node-fetch')
+
 const home = (req, res) => {
   res.render('pages/index', {/* vars here*/});
 };
 
 const myWork = (req, res) => {
-  res.render('pages/my-work', {/* vars here*/});
+  const username = "Bram-ter"
+  let url = `https://api.github.com/users/${username}/repos`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(repos => {
+      console.log(repos)
+      res.render('pages/my-work', { repos });
+    })
+    .catch(error => console.error(error));
 };
 
 module.exports = {
