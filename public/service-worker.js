588dfb0da4e94750de36498350ecb98a033e4fa7
Bram-ter <bram.tersteeg2@hva.nl>
2023-04-02 23:21:29 +0200

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,73 @@
 const CACHE_NAME = 'site-cache-v1';
+const CORE_ASSETS = [
+  '/offline',
 
-self.addEventListener('install', function(event) {
+  'css/style.css',
+  'css/buttons.css',
+
+  'js/enable-sw.js',
+
+  'manifest.json',
+
+  'assets/room_illustration.svg',
+  'assets/linkedin.svg',
+  'assets/github.svg',
+  'fonts/Merriweather/Merriweather-Light.ttf',
+  'fonts/Hema_Futura/FuturaHEMAProOT-Book.otf'
+]
+
+// * Install * //
+self.addEventListener('install', (event) => {
+  console.log("Installed");
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/my-work',
-          '/offline',
-
-          'css/style.css',
-          'css/buttons.css',
-
-          'js/enable-sw.js',
-          
-          'assets/room_illustration.svg',
-          'assets/linkedin.svg',
-          'assets/github.svg',
-          'fonts/Merriweather/Merriweather-Light.ttf',
-          'fonts/Hema_Futura/FuturaHEMAProOT-Book.otf'
-        ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+      .then(() => {
+        return self.clients.matchAll({ type: 'window' });
+      })
+      .then((clients) => {
+        clients.forEach((client) => {
+          client.navigate(client.url);
+        });
+      })
   );
 });
 
-// Activate the service worker
-self.addEventListener('activate', function(event) {
-  console.log('Service worker activated');
+
+// * Activate * //
+self.addEventListener("activate", (event) => {
+  console.log("Activated")
+  event.waitUntil(clients.claim())
 });
 
-self.addEventListener("fetch", (e) => {
-  console.log("[Service Worker] Fetching...")
-  e.respondWith(
-      caches.match(e.request)
-          .then(cachedResponse => {
-
-              if (cachedResponse) return cachedResponse
-
-              return fetch(e.request)
-                  .then(response => {
-                      // If the request is not a GET request,
-                      // OR if the request does not start with 'http'
-                      // don't cache the response
-                      if (e.request.method !== 'GET' || e.request.url.indexOf('http') !== 0) return response
-
-                      // Cache the response
-                      return caches.open(CACHE_NAME)
-                          .then(cache => {
-                              cache.put(e.request, response.clone())
-                              return response
-                          })
-                  })
-                  
-          })
-          .catch(() => caches.open(CACHE_NAME)
-           .then(cache => 
-            cache.match('/offline'))
-      )
-  )
-})
+// * Fetching * //
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  console.log("Fetching:" + req.url);
+  
+  event.respondWith(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.match(req).then((cachedRes) => {
+          if (cachedRes) {
+            return cachedRes;
+          }
+          return fetch(req).then((fetchRes) => {
+            if (
+              !fetchRes ||
+              fetchRes.status !== 200 ||
+              fetchRes.type !== "basic"
+            ) {
+              return fetchRes;
+            }
+            cache.put(req, fetchRes.clone());
+            return fetchRes;
+          });
+        });
+      })
+      .catch(() => {
+        return caches.match('/offline');
+      })
+  );
+});
