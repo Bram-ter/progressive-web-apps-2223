213bcfe642fc935b99517cb3f46624c7b71d827d
Bram-ter <bram.tersteeg2@hva.nl>
2023-04-06 11:44:54 +0200

    tweak server

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const https = require("https");
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
+let options = { maxAge: '2y'}
 
 const cert = fs.readFileSync("localhost.pem", "utf-8");
 const key = fs.readFileSync("localhost-key.pem", "utf-8");
 
-const ejs = require("ejs");
-
 const routes = require("./routes/routes");
 
 /* Middleware */
-app.use(express.static("./public"));
+app.use(express.static("./public", options));
 
 app.use(function(req, res, next) {
     // Add the req object to res.locals, so it can be accessed in subsequent middleware functions and routes
